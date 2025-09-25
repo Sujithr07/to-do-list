26b8aaa595d53a8c8f80dd7cafc7d4ab031370db
Sujith <sujithj7123@gmail.com>
2025-09-25 22:34:31 +0530

    mod login for demo

---
+++ todolist/authentication/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def register(request):
+    if request.method =='POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirm_password')
+
+
+    return render(request,'authentication/register.html')
+
+
+def login_user(request):
+    return render(request,'authentication/login.html')
+    
