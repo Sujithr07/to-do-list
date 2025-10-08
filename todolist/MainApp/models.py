8ba2fcad54b32f0a7a3a10894848b3714060812a
Sujith <sujithj7123@gmail.com>
2025-10-08 10:27:14 +0530

    created models in mainapp

--- todolist/MainApp/models.py
+++ todolist/MainApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
+class Task(models.Model):
+    user =models.ForeignKey(User, on_delete=models.CASCADE)
+    content=models.CharField(max_length=200)
+    is_completed=models.BooleanField(default=False)
+    timestamp=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Todo: {self.content} | Completed: {self.is_completed}"
 
