7cbab105e20c095bdd9a207206c028bd15f61e06
Sujith <sujithj7123@gmail.com>
2025-10-08 12:07:57 +0530

    homepage, logout

--- todolist/authentication/urls.py
+++ todolist/authentication/urls.py
@@ -2,7 +2,7 @@ from .import views
 from django.urls import path
 
 urlpatterns=[
-    path('',views.home,name='home'),
+    path('test-home/',views.home,name='test-home'),
     path('register/',views.register, name='register'),
     path('login/',views.login_user, name='login'),
     path('logout/',views.logout_user, name='logout')
