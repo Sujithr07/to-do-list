11ca62710ae86573a2e0f910a3678532407b1d97
Sujith <sujithj7123@gmail.com>
2025-10-16 19:02:06 +0530

    create tasks

--- todolist/MainApp/views.py
+++ todolist/MainApp/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-
+from .models import Todo
+from django.contrib import messages
 
 @login_required
 def home(request):
+
+    if request.method=='POST':
+        task=request.POST.get('task')
+
+        new_task = Todo(
+            user = user,
+            content = task
+        )
+        new_task.save()
+
+        messages.success(request, 'New task saved successfully!')
+        return redirect('home')
+
+    user_tasks = Todo.objects.filter(user=request.user).order_by('-timestamp')
+
+    finished_tasks = user_tasks.filter(is_completed=True).count()
+    unfinished_tasks = user_tasks.filter(is_completed=False).count()
+
+    context = {
+        'tasks': user_tasks,
+        'finished_tasks': finished_tasks,
+        'unfinished_tasks': unfinished_tasks
+    }
+
     return render(request,'homepage/index.html')
